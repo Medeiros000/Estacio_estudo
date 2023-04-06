bc2ef8cbd0ed0908607cb5a1619df7894397dfc1
Medeiros000 <98979459+Medeiros000@users.noreply.github.com>
2023-04-06 16:50:04 -0300

    modulo 5

---
+++ Estudo_C/LeSequenciaNumericaEMostreNumerosLidos.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int num;
+    do
+    {
+        printf ("Digite um número: ");
+        scanf("%d",&num);
+
+        if (num!=0)
+            printf ("O número lido foi = %d\n\n ",num);        
+    }
+    while (num!=0);
+    return 0;
+}
---
+++ Estudo_C/SomaSeParSubtraiSeImpar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int num, resultado;
+    do
+    {
+        printf ("\nDigite um número: ");
+        scanf("%d",&num);
+
+        if (num!=0){
+            if (num%2==0){
+                resultado = num + 1;
+                printf ("\n%d e par, o resultado e = %d",num,resultado);
+            } else {
+                resultado = num - 1;
+                printf ("\n%d e impar, o resultado e = %d",num,resultado);
+            }
+        }
+    } while (num!=0);
+}    
